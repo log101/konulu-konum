e6b140068b633ae7508572e1f5ac8f9d4d9fdb26
log101 <ffrknerdm@gmail.com>
2024-07-23 22:32:26 +0300

    feat: add css transition for unlock buton

--- src/scripts/lockedContent.ts
+++ src/scripts/lockedContent.ts
@@ -8,6 +8,8 @@ let watchId: number;
 const targetLocation = document.getElementById("locked-content-container")
   ?.dataset.targetPosition;
 
+// This element display the distance between the user and the
+// target if geolocation permission is granted
 const descriptionElement = document.getElementById(
   "locked-content-description"
 );
@@ -16,8 +18,9 @@ const locationPermissionButton = document.getElementById(
   "location-permission-button"
 );
 
+// Generates a human readable destination text according to
+// distance remaining
 function generateDistanceText(distance: number) {
-  // Update the proximity text according to the distance remaining
   if (distance > 1000) {
     return `${(distance / 1000).toFixed()} KM`;
   } else if (distance > 100) {
@@ -25,23 +28,53 @@ function generateDistanceText(distance: number) {
   }
 }
 
+// Update the elements according to distance remaining
 function updateCurrentLocation(position: GeolocationPosition) {
   const newPosition = position.coords;
 
   if (targetLocation) {
+    // Calculate the distance remaining
     const distance = calculateDistance(
       [newPosition.latitude, newPosition.longitude],
       JSON.parse(targetLocation)
     );
+    if (distance < 100) {
+      // If user has arrived to destination
+      // Transform locked button to reveal button
+      const unlockButton = document.getElementById("unlock-content-button");
+      const unlockIcon = document.getElementById("unlock-icon");
+      const lockIcon = document.getElementById("lock-icon");
+      const buttonText = document.getElementById("button-text");
 
-    const distanceText = generateDistanceText(distance);
+      if (unlockButton) {
+        if (buttonText) buttonText.innerText = "İçeriği Göster";
+        if (lockIcon) lockIcon.classList.add("hidden");
+        if (unlockIcon) unlockIcon.classList.remove("hidden");
+        unlockButton.classList.remove("bg-primary", "hover:bg-primary/90");
+        unlockButton.classList.add(
+          "transition-[background-color]",
+          "duration-1000"
+        );
+        unlockButton.classList.add(
+          "animate-pulse",
+          "bg-indigo-600",
+          "hover:bg-indigo-700",
+          "hover:animate-none",
+          "border-2",
+          "border-indigo-800"
+        );
+      }
+    } else {
+      const distanceText = generateDistanceText(distance);
 
-    if (descriptionElement)
-      descriptionElement.innerText = `Kalan mesafe: ${distanceText}`;
+      if (descriptionElement)
+        descriptionElement.innerText = `Kalan mesafe: ${distanceText}`;
+    }
 
     if (locationPermissionButton) locationPermissionButton.remove();
   }
 
+  // Update leaflet controls
   onLocationSuccess(position);
 }
 
@@ -63,21 +96,19 @@ lockedContentContainer?.addEventListener(
   startWatchingLocation
 );
 
-navigator.permissions
-  .query({ name: "geolocation" })
-  .then((permissionStatus) => {
-    switch (permissionStatus.state) {
-      case "granted":
-        watchId = window.navigator.geolocation.watchPosition(
-          updateCurrentLocation,
-          errorCallback
-        );
-        break;
-      case "denied":
-      case "prompt":
-      default:
-        break;
-    }
-  });
+navigator.permissions.query({ name: "geolocation" }).then(permissionStatus => {
+  switch (permissionStatus.state) {
+    case "granted":
+      watchId = window.navigator.geolocation.watchPosition(
+        updateCurrentLocation,
+        errorCallback
+      );
+      break;
+    case "denied":
+    case "prompt":
+    default:
+      break;
+  }
+});
 
 export { startWatchingLocation };
