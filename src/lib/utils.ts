9e798f1b15fe3183b0e4e2d42a641f0e3d84de54
log101 <ffrknerdm@gmail.com>
2024-07-25 19:53:59 +0300

    refactor: remove stale code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
-export function remoteLog(data: any) {
-  fetch("/api/debug", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(data) })
+  return twMerge(clsx(inputs));
 }
