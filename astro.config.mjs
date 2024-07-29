6ea439022327ad60612744b8ff294557955df973
log101 <ffrknerdm@gmail.com>
2024-07-29 19:20:05 +0300

    chore: remove stale import statement

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
