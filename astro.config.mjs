0330439362c5b785958ec32e1a2f5119236e4e8e
log101 <ffrknerdm@gmail.com>
2024-08-06 13:46:28 +0300

    chore: change site on dev mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+const devMode = import.meta.env.DEV
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
     applyBaseStyles: false
   })],
   output: "static",
-  site: "https://konulukonum.log101.dev"
+  site: devMode ? "http://localhost:4321" : "https://konulukonum.log101.dev"
 });
--- src/components/Leaflet/icons.ts
+++ src/components/Leaflet/icons.ts
@@ -1,12 +1,12 @@
 import L from "leaflet"
 
 var targetLocationIcon = L.icon({
-  iconUrl: "https://konulukonum.log101.dev/goal.svg",
+  iconUrl: "/goal.svg",
   iconSize: [32, 32],
 })
 
 var currentLocationIcon = L.icon({
-  iconUrl: "https://konulukonum.log101.dev/blue-dot.png",
+  iconUrl: "/blue-dot.png",
   iconSize: [32, 32],
 })
 
